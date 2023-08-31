c4d96cf523a63883d5513f954fb5fcc020886e6f
Navegador <Navegador@RLG10726-71018>
2023-08-31 20:17:24 -0300

    Agregar archivos de proyecto.

---
+++ Ejemplo001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejemplo001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            string cadena = "Saludo 2";
+            Saludo();
+            //Thread.Sleep(10000);
+            Console.ReadKey();
+
+        }
+
+        private static void Saludo()
+        {
+            Console.WriteLine("Hola Mundo...Otra vez...");
+        }
+    }
+}
