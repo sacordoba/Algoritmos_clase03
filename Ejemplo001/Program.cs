a85af37fe4a14a2887f81775010780966c7e0fed
sacordoba <sacordoba54341@gmail.com>
2023-08-31 20:50:41 -0300

    for / arrays / cadenas

--- Ejemplo001/Program.cs
+++ Ejemplo001/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,22 @@ namespace Ejemplo001
         {
             Console.WriteLine("Hola Mundo");
             string cadena = "Saludo 2";
+
+            int[] numeros = new int[4];
+            int[] numeros1 = new int[] {10,20,30,40} ;
+            List<int> lista = new List<int>();
+            
+            for (int i = 0; i < 4; i++)
+            {
+                numeros[i] = numeros1[3-i];
+                lista.Add(numeros1[3-i]);
+            }
+
+            foreach(int item in lista) {
+                Console.WriteLine("El nº es: " + item);
+                Console.WriteLine("El nº es: {0}", item);
+                Console.WriteLine($"El nº es: {item}");
+            }
 
             Saludo(cadena);
             //Thread.Sleep(10000);
