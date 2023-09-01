e146886201393a7db8026f58c018ec2eeb5a5d3a
sacordoba <sacordoba54341@gmail.com>
2023-08-31 21:43:14 -0300

    actulizado codigo de ejemplo 2

--- Ejemplo001/Program.cs
+++ Ejemplo001/Program.cs
@@ -15,22 +15,6 @@ namespace Ejemplo001
             Console.WriteLine("Hola Mundo");
             string cadena = "Saludo 2";
 
-            int[] numeros = new int[4];
-            int[] numeros1 = new int[] {10,20,30,40} ;
-            List<int> lista = new List<int>();
-            
-            for (int i = 0; i < 4; i++)
-            {
-                numeros[i] = numeros1[3-i];
-                lista.Add(numeros1[3-i]);
-            }
-
-            foreach(int item in lista) {
-                Console.WriteLine("El nº es: " + item);
-                Console.WriteLine("El nº es: {0}", item);
-                Console.WriteLine($"El nº es: {item}");
-            }
-
             Saludo(cadena);
             //Thread.Sleep(10000);
             Console.ReadKey();
--- Ejemplo002/Program.cs
+++ Ejemplo002/Program.cs
@@ -10,6 +10,25 @@ namespace Ejemplo002
     {
         static void Main(string[] args)
         {
+            int[] numeros = new int[4];
+            int[] numeros1 = new int[] { 10, 20, 30, 40 };
+            List<int> lista = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                numeros[i] = numeros1[3 - i];
+                lista.Add(numeros1[3 - i]);
+            }
+
+            foreach (int item in lista)
+            {
+                Console.WriteLine("El nº es: " + item);
+                Console.WriteLine("El nº es: {0}", item);
+                Console.WriteLine($"El nº es: {item}");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
